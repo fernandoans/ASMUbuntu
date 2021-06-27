565a2629e83899501036759f770da63bbfd30109
Fernando Anselmo <fernando.anselmo74@gmail.com>
2021-06-26 21:36:39 -0300

    [Add] Aula 01 - Hello World

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ASMUbuntu
+
+Este é o conteúdo das aulas de Assembly na Prática com o Ubuntu disponível gratuitamente no meu canal do YouTube.
+
+Todos os programas são de uso livre e irrestrito.
---
+++ aula01/HelloWorld.s
@@ -0,0 +1,28 @@
+# Mostrar um Hello World!
+#
+# as -o HelloWorld.o HelloWorld.s
+# ld -o HelloWorld HelloWorld.o
+
+.section .data
+	msg:	.string	"Hello World Olá\n"
+	tam:	.long	. - msg
+
+.section .bss
+
+.section .text
+
+.global _start
+
+_start:
+	mov $0x4, %rax
+	mov $0x1, %rbx
+	lea msg, %rcx
+	mov tam, %rdx
+	int $0x80
+	
+	mov $0x1, %rax
+	mov $0x0, %rbx
+	int $0x80
+	
+	
+	
